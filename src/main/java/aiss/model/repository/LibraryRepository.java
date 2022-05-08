69350751e443665b116c1b4b6f2d3e71cb9efe64
ismbargar <ismbargar@alum.us.es>
2022-05-08 14:26:46 +0200

    Merge branch 'main' of https://github.com/ismbargar/AISS-Integracion.git into main

--- src/main/java/aiss/model/repository/LibraryRepository.java
+++ src/main/java/aiss/model/repository/LibraryRepository.java
@@ -8,24 +8,13 @@ import aiss.model.Film;
 public interface LibraryRepository {
 	
 	
-	// Songs
 	public void addFilm(Film s);
 	public Collection<Film> getAllFilms();
 	public Film getFilm(String filmId);
 	public void updateFilm(Film s);
 	public void deleteFilm(String filmId);
 	
-	// Playlists
-<<<<<<< HEAD
-	public void addLibrary(Library p);
-	public Collection<Library> getAllLibraries();
-	public Library getLibrary(String playlistId);
-	public void updateLibrary(Library p);
-	public void deleteLibrary(String playlistId);
-	public Collection<Film> getAll(String playlistId);
-	public void addFilm(String playlistId, String songId);
-	public void removeFilm(String playlistId, String songId); 
-=======
+
 	public void addLibrary(Library l);
 	public Collection<Library> getAllLibraries();
 	public Library getLibrary(String libraryId);
@@ -34,7 +23,7 @@ public interface LibraryRepository {
 	public Collection<Film> getAll(String libraryId);
 	public void addFilm(String libraryId, String filmId);
 	public void removeFilm(String libraryId, String filmId); 
->>>>>>> branch 'main' of https://github.com/ismbargar/AISS-Integracion.git
+
 
 	
 	
